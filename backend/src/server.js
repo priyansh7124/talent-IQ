5af56045183dbce443522de1d9a421ad8b3abb1a
Priyansh Agrawal <priyanshagrawal806@gmail.com>
2025-12-18 00:50:03 +0530

    Made it deployment ready by ensuring a single local server for both frontend and backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,27 @@
 import express from "express";
-import dotenv from "dotenv";
+import path from "path";
+
 import { ENV } from "./lib/env.js";
 
-dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Success from ap i" });
+const __dirname = path.resolve();
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-app.listen(ENV.PORT, () =>
-  console.log(`Connected with the server ${ENV.PORT}`)
-);
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is the books endpoint" });
+});
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
