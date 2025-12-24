bbbede98bb14be526010e11ae6000978d7727cdf
Priyansh Agrawal <priyanshagrawal806@gmail.com>
2025-12-24 22:15:30 +0530

    Navbar components with router as button and added animation to it

--- frontend/src/pages/ProblemsPage.jsx
+++ frontend/src/pages/ProblemsPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const ProblemsPage = () => {
-  return <div>Problems Page</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default ProblemsPage;
