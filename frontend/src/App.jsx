6165d1bd4b89d989a5e654b13c2bd47fcaf526f5
Priyansh Agrawal <priyanshagrawal806@gmail.com>
2025-12-24 20:32:39 +0530

    Home Page with the help of daisy ui but the theme is not set right now,left fot later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import { useUser } from "@clerk/clerk-react";
 import ProblemsPage from "./pages/ProblemsPage";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
 function App() {
   const { isSignedIn, isLoaded } = useUser();
-
+  // this will prevent the UI from flickering while Clerk loads and checks the auth state
   if (!isLoaded) return null;
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
